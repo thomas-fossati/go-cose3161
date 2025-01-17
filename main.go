215f16f0c318f57f3108c9fa5ee3bb0c54447281
Thomas Fossati <thomas.fossati@linaro.org>
2025-01-17 19:32:13 +0100

    update codepoint
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 	headers := cose.Headers{
 		Protected: cose.ProtectedHeader{
 			cose.HeaderLabelAlgorithm: cose.AlgorithmES256,
-			51:                        tst,
+			258:                       tst,
 		},
 		Unprotected: cose.UnprotectedHeader{
 			cose.HeaderLabelKeyID: []byte("11"),
